5a782fe31c77e69dc70ee3bb598d63fcd3d04023
Raul Cisneros <raul.f.cisneros@outlook.com>
2020-05-18 19:05:44 -0700

    More on Variables

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,35 +12,53 @@ namespace VARIABLES
     {
         static void Main(string[] args)
         {
-        //Console.WriteLine("My name is Raul  & I 'm 43 years old. <Press space to continue>");
-        //Console.ReadKey ();
-        //Console.WriteLine("\nMy favourite color is purple ....Very masculine.");
-        //Console.ReadKey();
+            //Console.WriteLine("My name is Raul  & I 'm 43 years old. <Press space to continue>");
+            //Console.ReadKey ();
+            //Console.WriteLine("\nMy favourite color is purple ....Very masculine.");
+            //Console.ReadKey();
 
-        //int num01;
-        //int num02;
+            //int num01;
+            //int num02;
 
-        //Console.Write("Type a number to be multiplied: ");
-        //num01 = Convert.ToInt32(Console.ReadLine());
-        //Console.WriteLine("Type another number: ");
-        //num02 = Convert.ToInt32(Console.ReadLine());
-        //Console.WriteLine("The result is  " + num01 * num02);
-        //Console.ReadKey();
+            //Console.Write("Type a number to be multiplied: ");
+            //num01 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Type another number: ");
+            //num02 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("The result is  " + num01 * num02);
+            //Console.ReadKey();
 
-        //Start:
-        //    int num01;
-        //    int num02;
+            //Start:
+            //    int num01;
+            //    int num02;
 
-        //    Console.WriteLine("Type a number to be divided: ");
-        //    num01 = Convert.ToInt32(Console.ReadLine());
-        //    Console.WriteLine("Type another number: " );
-        //    num02 = Convert.ToInt32(Console.ReadLine());
-        //    Console.WriteLine(num01 + "divided by " + num02 + " is equal to " + num01 / num02);
+            //    Console.WriteLine("Type a number to be divided: ");
+            //    num01 = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Type another number: " );
+            //    num02 = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine(num01 + "divided by " + num02 + " is equal to " + num01 / num02);
 
-        //    //wait for the user to press a key. Then make empty space and start over.
-        //    Console.ReadKey();
-        //    Console.WriteLine();
-        //    goto Start; //Jumps to  "Start:".
+            //    //wait for the user to press a key. Then make empty space and start over.
+            //    Console.ReadKey();
+            //    Console.WriteLine();
+            //    goto Start; //Jumps to  "Start:".
+
+
+            //x + 7;
+            //y + x + 3;
+            //Console.WriteLine(y);
+
+            //int x;
+            //int y;
+            //x = 7;
+            //y = x + 3;
+            //Console.WriteLine(y);
+            //Console.ReadLine();
+
+            //var x = 7;
+            //var y = x + 3;
+            //Console.WriteLine(y);
+
+
 
         }
     }
